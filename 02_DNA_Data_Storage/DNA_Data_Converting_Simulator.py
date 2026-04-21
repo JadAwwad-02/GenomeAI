9649dc454608930c2e628375769c86f86e9ff43d
JadAwwad-02 <jadawwad199@email.com>
2026-04-21 23:04:26 +0200

    organize projects and add NCBI fetcher# Please enter the commit message for your changes. Lines starting

--- 02_DNA_Data_Storage/DNA_Data_Converting_Simulator.py
+++ 02_DNA_Data_Storage/DNA_Data_Converting_Simulator.py
@@ -5,16 +5,35 @@ encoding = {
     "11": "C"
 }
 
+reverse_encoding = {
+    "A": "00",
+    "T": "01",
+    "G": "10",
+    "C": "11"
+}
+
+
 def text_to_dna(text):
     dna = ""
-    
     for char in text:
         binary = format(ord(char), '08b')
-        dna += ''.join(encoding[binary[i:i+2]] for i in range(0,8,2))
-    
+        dna += ''.join(encoding[binary[i:i+2]] for i in range(0, 8, 2))
     return dna
 
 
-sequence = input("Input text: ")
+def dna_to_text(dna_sequence):
+    result = ""
+    for i in range(0, len(dna_sequence), 4):
+        group = dna_sequence[i:i+4]
+        if len(group) < 4:
+            break
+        binary = ''.join(reverse_encoding[nuc] for nuc in group)
+        result += chr(int(binary, 2))
+    return result
 
+
+sequence = input("Input text: ")
 print("DNA:", text_to_dna(sequence))
+
+dna_input = input("Input DNA: ")
+print("Text:", dna_to_text(dna_input))
---
+++ 03_Bioinformatics/ncbi_gene_fetcher.py
@@ -0,0 +1,5 @@
+"""
+NCBI Gene Fetcher
+Connects to NCBI via the Entrez API and downloads real gene sequences.
+Requires: pip install biopython
+"""
